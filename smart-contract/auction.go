881fc071e3f7a56bb425f476d0c8a69d0e49c03f
restlesshome <restlesswhy@gmail.com>
2022-05-11 01:19:27 +0200

    +

---
+++ smart-contract/auction.go
@@ -0,0 +1,244 @@
+/*
+SPDX-License-Identifier: Apache-2.0
+*/
+
+package auction
+
+import (
+	"encoding/json"
+	"fmt"
+
+	"github.com/hyperledger/fabric-contract-api-go/contractapi"
+)
+
+type SmartContract struct {
+	contractapi.Contract
+}
+
+// Thread data
+type Thread struct {
+	Category    string              `json:"category"`
+	Theme       string              `json:"theme"`
+	Description string              `json:"description"`
+	Creator     string              `json:"creator"`
+	Options     map[string][]string `json:"options"`
+	WinOption   []string            `json:"win_option"`
+	Status      string              `json:"status"`
+}
+
+const voteKeyType = "vote"
+
+// CreateAuction creates on auction on the public channel. The identity that
+// submits the transacion becomes the seller of the auction
+func (s *SmartContract) CreateThread(ctx contractapi.TransactionContextInterface) error {
+
+	args := ctx.GetStub().GetStringArgs()
+	if len(args) < 4 {
+		return fmt.Errorf("not enough arguments")
+	}
+
+	threadID := args[1]
+	category := args[2]
+	theme := args[3]
+	description := args[4]
+	options := args[5:]
+
+	res, err := ctx.GetStub().GetState(threadID)
+	if err != nil {
+		return fmt.Errorf("failed to get thread %v", err)
+	} else if res != nil {
+		return fmt.Errorf("failed to create, thread ID already exist")
+	}
+
+	clientID, err := s.GetSubmittingClientIdentity(ctx)
+	if err != nil {
+		return fmt.Errorf("failed to get client identity %v", err)
+	}
+
+	// get org of submitting client
+	clientOrgID, err := ctx.GetClientIdentity().GetMSPID()
+	if err != nil {
+		return fmt.Errorf("failed to get client identity %v", err)
+	}
+
+	// Create tread
+	threadOptions := make(map[string][]string)
+
+	for _, option := range options {
+		threadOptions[option] = make([]string, 0)
+	}
+
+	tread := Thread{
+		Category: category,
+		Theme: theme,
+		Description: description,
+		Creator:   clientID,
+		Options:   threadOptions,
+		WinOption: []string{},
+		Status:    "open",
+	}
+
+	auctionJSON, err := json.Marshal(tread)
+	if err != nil {
+		return err
+	}
+
+	// put auction into state
+	err = ctx.GetStub().PutState(threadID, auctionJSON)
+	if err != nil {
+		return fmt.Errorf("failed to put auction in public data: %v", err)
+	}
+
+	// set the creator of the tread as an endorser
+	err = setAssetStateBasedEndorsement(ctx, threadID, clientOrgID)
+	if err != nil {
+		return fmt.Errorf("failed setting state based endorsement for new organization: %v", err)
+	}
+
+	return nil
+}
+
+// Bid is used to add a user's bid to the auction. The bid is stored in the private
+// data collection on the peer of the bidder's organization. The function returns
+// the transaction ID so that users can identify and query their bid
+func (s *SmartContract) CreateVote(ctx contractapi.TransactionContextInterface, threadID string) (string, error) {
+
+	// get the implicit collection name using the voter organization ID
+	collection, err := getCollectionName(ctx)
+	if err != nil {
+		return "", fmt.Errorf("failed to get implicit collection name: %v", err)
+	}
+
+	// the transaction ID is used as a unique index for the vote
+	txID := ctx.GetStub().GetTxID()
+
+	// create a composite key using the transaction ID
+	voteKey, err := ctx.GetStub().CreateCompositeKey(voteKeyType, []string{threadID, txID, collection})
+	if err != nil {
+		return "", fmt.Errorf("failed to create composite key: %v", err)
+	}
+
+	err = ctx.GetStub().PutState(voteKey, []byte("vote"))
+	if err != nil {
+		return "", fmt.Errorf("failed to input vote into collection: %v", err)
+	}
+
+	return txID, nil
+}
+
+// SubmitBid is used by the bidder to add the hash of that bid stored in private data to the
+// auction. Note that this function alters the auction in private state, and needs
+// to meet the auction endorsement policy. Transaction ID is used identify the bid
+func (s *SmartContract) UseVote(ctx contractapi.TransactionContextInterface, treadID string, txID string, option string) error {
+
+	// get the tread from public state
+	tread, err := s.QueryThread(ctx, treadID)
+	if err != nil {
+		return fmt.Errorf("failed to get auction from public state %v", err)
+	}
+
+	// tread needs to be open for users to add their vote
+	Status := tread.Status
+	if Status != "open" {
+		return fmt.Errorf("cannot join closed or ended auction")
+	}
+
+	// get the inplicit collection name of voter org
+	collection, err := getCollectionName(ctx)
+	if err != nil {
+		return fmt.Errorf("failed to get implicit collection name: %v", err)
+	}
+
+	// что если ключ будет привязан к никнейму подписанта
+	// use the transaction ID passed as a parameter to create composite vote key
+	voteKey, err := ctx.GetStub().CreateCompositeKey(voteKeyType, []string{treadID, txID, collection})
+	if err != nil {
+		return fmt.Errorf("failed to create composite key: %v", err)
+	}
+
+	_, err = ctx.GetStub().GetState(voteKey)
+	if err != nil {
+		return fmt.Errorf("failed to get vote: %v", err)
+	}
+
+	// Проверка наличия варианта в вариантах ответа
+	if !contains(tread.Options, option) {
+		return fmt.Errorf("failed to use vote: unexpected option")
+	}
+
+	// Получаем айди пользователя
+	userID, _, err := ctx.GetClientIdentity().GetAttributeValue("hf.EnrollmentID")
+	if err != nil {
+		return err
+	}
+
+	// Добавляем голос к выбранному варианту
+	tread.Options[option] = append(tread.Options[option], userID)
+
+	// Переводим в джсон обновленный тред
+	newThreadJSON, err := json.Marshal(tread)
+	if err != nil {
+		return err
+	}
+
+	err = ctx.GetStub().PutState(treadID, newThreadJSON)
+	if err != nil {
+		return fmt.Errorf("failed to update auction: %v", err)
+	}
+
+	return nil
+}
+
+
+// EndAuction both changes the auction status to closed and calculates the winners
+// of the auction
+func (s *SmartContract) EndThread(ctx contractapi.TransactionContextInterface, threadID string) error {
+
+	// get thread from public state
+	thread, err := s.QueryThread(ctx, threadID)
+	if err != nil {
+		return fmt.Errorf("failed to get auction from public state %v", err)
+	}
+
+	// get username of submitting client
+	clientID, err := s.GetSubmittingClientIdentity(ctx)
+	if err != nil {
+		return fmt.Errorf("failed to get client identity %v", err)
+	}
+
+	Seller := thread.Creator
+	if Seller != clientID {
+		return fmt.Errorf("auction can only be ended by seller: %v", err)
+	}
+
+	Status := thread.Status
+	if Status != "open" {
+		return fmt.Errorf("cannot close auction that is not open")
+	}
+
+	thread.Status = string("closed")
+
+	voteAmount := 0
+	winOptions := make([]string, 0)
+	for k, v := range thread.Options {
+		if len(v) > voteAmount {
+			winOptions = append(winOptions, k)
+			winOptions = winOptions[len(winOptions)-1:]
+
+			voteAmount = len(v)
+		} else if len(v) == voteAmount {
+            winOptions = append(winOptions, k)
+        }
+	}
+
+	thread.WinOption = winOptions
+	thread.Status = string("ended")
+
+	endedAuctionJSON, _ := json.Marshal(thread)
+
+	err = ctx.GetStub().PutState(threadID, endedAuctionJSON)
+	if err != nil {
+		return fmt.Errorf("failed to end auction: %v", err)
+	}
+	return nil
+}
---
+++ smart-contract/utils.go
@@ -0,0 +1,73 @@
+/*
+SPDX-License-Identifier: Apache-2.0
+*/
+
+package auction
+
+import (
+	"fmt"
+	"encoding/base64"
+
+	"github.com/hyperledger/fabric-chaincode-go/pkg/statebased"
+	"github.com/hyperledger/fabric-contract-api-go/contractapi"
+)
+
+func (s *SmartContract) GetSubmittingClientIdentity(ctx contractapi.TransactionContextInterface) (string, error) {
+
+	b64ID, err := ctx.GetClientIdentity().GetID()
+	if err != nil {
+		return "", fmt.Errorf("failed to read clientID: %v", err)
+	}
+	decodeID, err := base64.StdEncoding.DecodeString(b64ID)
+	if err != nil {
+		return "", fmt.Errorf("failed to base64 decode clientID: %v", err)
+	}
+	return string(decodeID), nil
+}
+
+// setAssetStateBasedEndorsement sets the endorsement policy of a new auction
+func setAssetStateBasedEndorsement(ctx contractapi.TransactionContextInterface, auctionID string, orgToEndorse string) error {
+
+	endorsementPolicy, err := statebased.NewStateEP(nil)
+	if err != nil {
+		return err
+	}
+	err = endorsementPolicy.AddOrgs(statebased.RoleTypePeer, orgToEndorse)
+	if err != nil {
+		return fmt.Errorf("failed to add org to endorsement policy: %v", err)
+	}
+	policy, err := endorsementPolicy.Policy()
+	if err != nil {
+		return fmt.Errorf("failed to create endorsement policy bytes from org: %v", err)
+	}
+	err = ctx.GetStub().SetStateValidationParameter(auctionID, policy)
+	if err != nil {
+		return fmt.Errorf("failed to set validation parameter on auction: %v", err)
+	}
+
+	return nil
+}
+
+// getCollectionName is an internal helper function to get collection of submitting client identity.
+func getCollectionName(ctx contractapi.TransactionContextInterface) (string, error) {
+
+	// Get the MSP ID of submitting client identity
+	clientMSPID, err := ctx.GetClientIdentity().GetMSPID()
+	if err != nil {
+		return "", fmt.Errorf("failed to get verified MSPID: %v", err)
+	}
+
+	// Create the collection name
+	orgCollection := "_implicit_org_" + clientMSPID
+
+	return orgCollection, nil
+}
+
+func contains(sli map[string][]string, str string) bool {
+	for k := range sli {
+		if k == str {
+			return true
+		}
+	}
+	return false
+}
